7ab4d9b64c6eeb77677e1ed3e74b250d726fb77f
JaeHyeok <morest432@gmail.com>
2024-07-08 10:56:58 +0900

    fix: 게시글조회 수정

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mysql = require("mysql");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
 
 // express
 const app = express();
@@ -14,21 +14,21 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // path 모듈 추가
-const path = require("path");
+const path = require('path');
 // serve-static 모듈 추가
-const static = require("serve-static");
+const static = require('serve-static');
 // http://localhost:5000/ 요청이 오면 was 서버의
 // public 폴더를 찾아서 그 안에 있는 index.html을 서비스하도록 설정
-app.use("/", static(path.join(__dirname, "public")));
+app.use('/', static(path.join(__dirname, 'public')));
 
 // db 접속
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "svc.sel5.cloudtype.app",
-  user: "root",
-  password: "1234",
-  port: "31635",
-  database: "pitstop",
+  host: 'svc.sel5.cloudtype.app',
+  user: 'root',
+  password: '1234',
+  port: '31635',
+  database: 'pitstop',
 });
 
 // 게시판 DB 가져오기
@@ -53,7 +53,7 @@ app.get(`/api/boardlist/:teamnum`, function (req, res) {
 });
 
 // board 조회
-/* app.get('/api/board/:id', (req, res) => {
+app.get('/api/board/:id', (req, res) => {
   const id = req.params.id;
 
   const sql = `select * from board where id = ?`;
@@ -76,18 +76,18 @@ app.get(`/api/boardlist/:teamnum`, function (req, res) {
       }
     });
   });
-}); */
+});
 
 // board 작성
-app.post("/api/boards", (req, res) => {
+app.post('/api/boards', (req, res) => {
   const { title, userid, content } = req.body;
 
   if (!title || !userid || !content) {
-    return res.status(400).send("제목, 작성자, 내용을 모두 입력하세요");
+    return res.status(400).send('제목, 작성자, 내용을 모두 입력하세요');
   }
 
   const sql =
-    "insert into board(title, userid, content, teamnum) values (?, ?, ?, 0)";
+    'insert into board(title, userid, content, teamnum) values (?, ?, ?, 0)';
 
   pool.getConnection((err, con) => {
     if (err) {
@@ -101,16 +101,16 @@ app.post("/api/boards", (req, res) => {
       }
 
       if (result.affectedRows > 0) {
-        res.json({ result: "success" });
+        res.json({ result: 'success' });
       } else {
-        res.json({ result: "fail" });
+        res.json({ result: 'fail' });
       }
     });
   });
 });
 
 // board 수정
-app.put("/api/boards/:id", (req, res) => {
+app.put('/api/boards/:id', (req, res) => {
   const id = req.params.id;
   const { title, userid, content } = req.body;
   const sql = `update board set title = ?, userid = ?, content = ? where id = ?`;
@@ -126,36 +126,36 @@ app.put("/api/boards/:id", (req, res) => {
       }
 
       if (result.affectedRows > 0) {
-        res.json({ result: "success" });
+        res.json({ result: 'success' });
       } else {
-        res.json({ result: "fail" });
+        res.json({ result: 'fail' });
       }
     });
   });
 });
 
 //--------------------------게시판 조회-------------------------------
-app.get("/api/boards", (req, res) => {
-  // offset 피라미터값 받기
-  let offset = req.query.offset;
-  if (!offset) {
-    offset = 0;
-  }
+// app.get("/api/boards", (req, res) => {
+//   // offset 피라미터값 받기
+//   let offset = req.query.offset;
+//   if (!offset) {
+//     offset = 0;
+//   }
 
-  // console.log('get /api/members')
-  const sql = `SELECT id, title, userid, content, readnum, date_format(wdate, '%Y-%m-%d') wdate from board `;
-  pool.getConnection((err, con) => {
-    if (err) return res.status(500).json(err); // db 연결오류
-    con.query(sql, (err, result) => {
-      con.release();
-      if (err) return res.status(500).json(err); // sql문 오류
-      res.json(result);
-    });
-  });
-});
+//   // console.log('get /api/members')
+//   const sql = `SELECT id, title, userid, content, readnum, date_format(wdate, '%Y-%m-%d') wdate from board `;
+//   pool.getConnection((err, con) => {
+//     if (err) return res.status(500).json(err); // db 연결오류
+//     con.query(sql, (err, result) => {
+//       con.release();
+//       if (err) return res.status(500).json(err); // sql문 오류
+//       res.json(result);
+//     });
+//   });
+// });
 
 // 글 보기
-app.get("/api/boards/:id", (req, res) => {
+app.get('/api/boards/:id', (req, res) => {
   const id = req.params.id;
   // console.log('id: ',id)
 
@@ -163,7 +163,7 @@ app.get("/api/boards/:id", (req, res) => {
   const sql =
     "SELECT id, title, userid, content, readnum, date_format(wdate, '%Y-%m-%d')wdate from board where id=?";
   pool.getConnection((err, con) => {
-    if (err) return res.status(500).send("Internal Server Error"); // db 연결오류
+    if (err) return res.status(500).send('Internal Server Error'); // db 연결오류
     con.query(sql, [id], (err, result) => {
       con.release();
       if (err) return res.status(500).json(err); // sql문 오류
@@ -186,9 +186,9 @@ app.post(`/api/boards/:id/reply`, (req, res) => {
       con.release();
       if (err) return res.status(500).send(err);
       if (result.affectedRows > 0) {
-        res.json({ result: "success" });
+        res.json({ result: 'success' });
       } else {
-        res.json({ result: "fail" });
+        res.json({ result: 'fail' });
       }
     });
   });
@@ -210,7 +210,7 @@ app.get(`/api/boards/:id/reply`, (req, res) => {
 
 app.delete(`/api/boards/reply/:rid`, (req, res) => {
   const rid = req.params.rid;
-  console.log("rid: ", rid);
+  console.log('rid: ', rid);
   const sql = `delete from reply where id=?`;
   pool.getConnection((err, con) => {
     if (err) return res.status(500).send(err);
@@ -218,9 +218,9 @@ app.delete(`/api/boards/reply/:rid`, (req, res) => {
       con.release();
       if (err) return res.status(500).send(err);
       if (result.affectedRows > 0) {
-        res.json({ result: "success" });
+        res.json({ result: 'success' });
       } else {
-        res.json({ result: "fail" });
+        res.json({ result: 'fail' });
       }
     });
   });
@@ -236,9 +236,9 @@ app.put(`/api/boards/reply/:rid`, (req, res) => {
       con.release();
       if (err) return res.status(500).send(err);
       if (result.affactedRows > 0) {
-        res.json({ result: "success" });
+        res.json({ result: 'success' });
       } else {
-        res.json({ result: "fail" });
+        res.json({ result: 'fail' });
       }
     });
   });
